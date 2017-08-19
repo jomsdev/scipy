c97993894d4bbd16a9ed2d784f2308daf0cb8b81
Jordi Montes <jordimontes@acm.org>
2017-08-19 12:08:08 +0200

    make_random_gaussian_matrix implemented
    Helper function for testing random projections

--- scipy/linalg/tests/test_random_projections.py
+++ scipy/linalg/tests/test_random_projections.py
@@ -1,6 +1,8 @@
 """Tests for functions in random_projections.py."""
 
 from __future__ import division, print_function, absolute_import
+import numpy as np
+from scipy.linalg import clarckson_woodruff_transform
 
 __usage__ = """
 Build linalg:
@@ -12,11 +14,18 @@ Run tests if scipy is installed:
 from numpy.testing import (assert_equal, assert_almost_equal,
                            assert_array_almost_equal, assert_array_equal,
                            assert_raises, assert_, assert_allclose)
+from nose.tools import (assert_true)
+
 
 import pytest
 
-from scipy.linalg import clarckson_woodruff_transform
+# Make some random data with Gaussian distributed values
+def make_random_gaussian_matrix(n_rows, n_columns, mu=0, sigma=0.01):
+    res = np.random.normal(mu, sigma, n_rows*n_columns)
+    return np.reshape(res, (n_rows, n_columns))
 
 class TestRandomProjections(object):
-    def check_clarkson_woodruff_transform():
-        assert_equal([1], [2])
+
+    def test_clarkson_woodruff_transform(self):
+        A = make_random_gaussian_matrix(500, 2000)
+        assert_true(A.shape == (500, 2000))
